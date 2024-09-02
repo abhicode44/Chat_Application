2d5b50faa2961850aaf9f25fdeb9dc8838d574a3
Abhishek Chudasama <120448024+abhicode44@users.noreply.github.com>
2024-09-02 16:41:37 +0530

    Update Register.js

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -28,7 +28,7 @@ function Register() {
     event.preventDefault();
     if (handleValidation()) {
       const { password, confirmpassword, username, email } = values;
-      const { data } = await axios.post("http://localhost:5000/api/auth/register", {
+      const { data } = await axios.post("https://chat-application-9sdd.onrender.com/api/auth/register", {
         username, email, password, confirmpassword
       });
       if (data.status === false) {
