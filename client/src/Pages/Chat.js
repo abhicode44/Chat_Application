04e3eb9d2e7a18aaedd926d9a3bc76af5da8b4a3
Abhishek Chudasama <120448024+abhicode44@users.noreply.github.com>
2024-09-02 23:05:23 +0530

    Update Again Link Chat.js

--- client/src/Pages/Chat.js
+++ client/src/Pages/Chat.js
@@ -8,7 +8,7 @@ import ChatContainer from "../Components/ChatContainer";
 import io from "socket.io-client";
 
 export default function Chat() {
-  const host = "http://localhost:5000";
+  const host = "https://chat-application-plew.onrender.com";
   const socket = useRef();
   const navigate = useNavigate();
   const [contacts, setContacts] = useState([]);
@@ -45,7 +45,7 @@ export default function Chat() {
       if (currentUser) {
         if (currentUser.isAvatarImageSet) {
           const { data } = await axios.get(
-            `http://localhost:5000/api/auth/allusers/${currentUser._id}`
+            `https://chat-application-plew.onrender.com/api/auth/allusers/${currentUser._id}`
           );
           setContacts(data);
         } else {
