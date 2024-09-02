498561116a3e925480a2dd65d38d5281a4f8c7ee
Abhishek Chudasama <120448024+abhicode44@users.noreply.github.com>
2024-09-02 19:14:40 +0530

    Update Again index.js

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ const server = app.listen(PORT, () => {
 // Setup socket.io with CORS configuration
 const io = socket(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "*",
         credentials: true,
     },
 });
